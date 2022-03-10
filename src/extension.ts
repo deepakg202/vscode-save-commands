0210ef37d2f70479fbef4674a64660501249b81f
Deepak Gupta <deepak@blend.to>
2022-03-10 09:20:15 +0530

    setKeysforSync for globalState

--- src/extension.ts
+++ src/extension.ts
@@ -8,12 +8,12 @@ export function activate(context: vscode.ExtensionContext) {
 	const treeView = new TreeDataProvider(context);
 	const addCommand = vscode.commands.registerCommand('save-commands.addCommand', function () {
 		vscode.window.showWarningMessage('Add Workspace Command');
-		utils.commandInput().then((val:any) => {
+		utils.commandInput().then((val: any) => {
 			let c = context.workspaceState.get('commands') as Array<object> || [];
 			const id = utils.uuidv4() as string;
-			c.push({id: id, name: val.name, command: val.cmd});
+			c.push({ id: id, name: val.name, command: val.cmd });
 			context.workspaceState.update('commands', c);
-			vscode.window.showInformationMessage('Added Workspace Command Successfully');	
+			vscode.window.showInformationMessage('Added Workspace Command Successfully');
 			vscode.commands.executeCommand('save-commands.refreshView');
 		}).catch((e: any) => {
 			vscode.window.showErrorMessage('Error Adding Command');
@@ -22,12 +22,13 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	const addGlobalCommand = vscode.commands.registerCommand('save-commands.addGlobalCommand', function () {
 		vscode.window.showWarningMessage('Add Global Command');
-		utils.commandInput().then((val:any) => {
+		utils.commandInput().then((val: any) => {
 			let c = context.globalState.get('commands') as Array<object> || [];
 			const id = utils.uuidv4() as string;
-			c.push({id: id, name: val.name, command: val.cmd});
+			c.push({ id: id, name: val.name, command: val.cmd });
 			context.globalState.update('commands', c);
-			vscode.window.showInformationMessage('Added Global Command Successfully');	
+			context.globalState.setKeysForSync(['commands']);
+			vscode.window.showInformationMessage('Added Global Command Successfully');
 			vscode.commands.executeCommand('save-commands.refreshView');
 		}).catch((e: any) => {
 			vscode.window.showErrorMessage('Error Adding Command');
@@ -38,119 +39,101 @@ export function activate(context: vscode.ExtensionContext) {
 	const deleteWorkspace = vscode.commands.registerCommand('save-commands.deleteWorkspaceCommands', () => {
 		context.workspaceState.update('commands', []);
 		vscode.commands.executeCommand('save-commands.refreshView');
-		vscode.window.showInformationMessage('Workspace Commands Deleted');	
+		vscode.window.showInformationMessage('Workspace Commands Deleted');
 	});
 	const deleteGlobal = vscode.commands.registerCommand('save-commands.deleteGlobalCommands', () => {
 		context.globalState.update('commands', []);
 		vscode.commands.executeCommand('save-commands.refreshView');
-		vscode.window.showInformationMessage('Global Commands Deleted');	
+		vscode.window.showInformationMessage('Global Commands Deleted');
 	});
 
 	const deleteCommand = vscode.commands.registerCommand('save-commands.deleteCommand', (item: TreeItem) => {
-		if(item.contextValue === 'child-global')
-		{
-				let c = context.globalState.get('commands') as Array<object> || [];
-				const i = c.findIndex((d:any) => d.id === item.cmdId);
-				if(i>-1)
-					{c.splice(i, 1);}
-				context.globalState.update('commands', c);
+		if (item.contextValue === 'child-global') {
+			let c = context.globalState.get('commands') as Array<object> || [];
+			const i = c.findIndex((d: any) => d.id === item.cmdId);
+			if (i > -1) { c.splice(i, 1); }
+			context.globalState.update('commands', c);
 		}
-		else if(item.contextValue === 'child-workspace')
-		{
-				let c = context.workspaceState.get('commands') as Array<object> || [];
-				const i = c.findIndex((d:any) => d.id === item.cmdId);
-				if(i>-1)
-					{c.splice(i, 1);}
-				context.workspaceState.update('commands', c);	
+		else if (item.contextValue === 'child-workspace') {
+			let c = context.workspaceState.get('commands') as Array<object> || [];
+			const i = c.findIndex((d: any) => d.id === item.cmdId);
+			if (i > -1) { c.splice(i, 1); }
+			context.workspaceState.update('commands', c);
 		}
 		vscode.commands.executeCommand('save-commands.refreshView');
 	});
 
 	const editCommand = vscode.commands.registerCommand('save-commands.editCommand', (item: TreeItem) => {
-		let c:any;	
-		try{
-			if(item.contextValue === 'child-workspace')
-			{
+		let c: any;
+		try {
+			if (item.contextValue === 'child-workspace') {
 				c = context.workspaceState.get('commands') as Array<object> || [];
 			}
-			else if(item.contextValue === 'child-global')
-			{
-				c = context.globalState.get('commands') as Array<object> || [];		
+			else if (item.contextValue === 'child-global') {
+				c = context.globalState.get('commands') as Array<object> || [];
 			}
-			const i = c.findIndex((d:any) => d.id === item.cmdId);
-			if(i>-1)
-			{
-				utils.commandInput({name: c[i].name, cmd: c[i].command}).then((val) => {
+			const i = c.findIndex((d: any) => d.id === item.cmdId);
+			if (i > -1) {
+				utils.commandInput({ name: c[i].name, cmd: c[i].command }).then((val) => {
 					c[i].name = val.name;
 					c[i].command = val.cmd;
-					if(item.contextValue === 'child-workspace')
-					{
+					if (item.contextValue === 'child-workspace') {
 						context.workspaceState.update('commands', c);
 					}
-					else if(item.contextValue === 'child-global')
-					{
+					else if (item.contextValue === 'child-global') {
 						context.globalState.update('commands', c);
 					}
 					vscode.commands.executeCommand('save-commands.refreshView');
 				});
 			}
-			else
-			{
+			else {
 				throw Error('Unable to find the command in state');
 			}
-		}catch(e) {
-			vscode.window.showErrorMessage('Unable to execute the command');
-		}
-	});
-	
-	const copyCommand = vscode.commands.registerCommand('save-commands.copyCommand', (item:TreeItem) => {
-		let c:any;	
-		try{
-			if(item.contextValue === 'child-workspace')
-			{
-				c = context.workspaceState.get('commands') as Array<object> || [];
-			}
-			else if(item.contextValue === 'child-global')
-			{
-				c = context.globalState.get('commands') as Array<object> || [];		
-			}
-			const i = c.findIndex((d:any) => d.id === item.cmdId);
-			if(i>-1)
-			{
-				vscode.env.clipboard.writeText(c[i].command);
-				vscode.window.showInformationMessage(`${c[i].name} Command Copied to Clipboard`);
-			}
-			else
-			{
-				throw Error('Unable to find the command in state');
-			}
-		}catch(e) {
+		} catch (e) {
 			vscode.window.showErrorMessage('Unable to execute the command');
 		}
 	});
 
-	const runCommand = vscode.commands.registerCommand('save-commands.runCommand', (item:TreeItem) => {
-		let c:any;	
-		try{
-			if(item.contextValue === 'child-workspace')
-			{
+	const copyCommand = vscode.commands.registerCommand('save-commands.copyCommand', (item: TreeItem) => {
+		let c: any;
+		try {
+			if (item.contextValue === 'child-workspace') {
 				c = context.workspaceState.get('commands') as Array<object> || [];
 			}
-			else if(item.contextValue === 'child-global')
-			{
-				c = context.globalState.get('commands') as Array<object> || [];		
+			else if (item.contextValue === 'child-global') {
+				c = context.globalState.get('commands') as Array<object> || [];
 			}
-			const i = c.findIndex((d:any) => d.id === item.cmdId);
-			if(i>-1)
-			{
+			const i = c.findIndex((d: any) => d.id === item.cmdId);
+			if (i > -1) {
+				vscode.env.clipboard.writeText(c[i].command);
+				vscode.window.showInformationMessage(`${c[i].name} Command Copied to Clipboard`);
+			}
+			else {
+				throw Error('Unable to find the command in state');
+			}
+		} catch (e) {
+			vscode.window.showErrorMessage('Unable to execute the command');
+		}
+	});
+
+	const runCommand = vscode.commands.registerCommand('save-commands.runCommand', (item: TreeItem) => {
+		let c: any;
+		try {
+			if (item.contextValue === 'child-workspace') {
+				c = context.workspaceState.get('commands') as Array<object> || [];
+			}
+			else if (item.contextValue === 'child-global') {
+				c = context.globalState.get('commands') as Array<object> || [];
+			}
+			const i = c.findIndex((d: any) => d.id === item.cmdId);
+			if (i > -1) {
 				term.sendText(c[i].command);
 				term.show();
 			}
-			else
-			{
+			else {
 				throw Error('Unable to find the command in state');
 			}
-		}catch(e) {
+		} catch (e) {
 			vscode.window.showErrorMessage('Unable to execute the command');
 		}
 	});
@@ -163,7 +146,7 @@ export function activate(context: vscode.ExtensionContext) {
 	const refreshView = vscode.commands.registerCommand('save-commands.refreshView', () => {
 		treeView.refresh();
 	});
-	
+
 	vscode.window.registerTreeDataProvider('save-commands-view', treeView);
 	context.subscriptions.push(addCommand, addGlobalCommand);
 	context.subscriptions.push(deleteWorkspace, deleteGlobal);
@@ -172,4 +155,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {term.dispose();}
+export function deactivate() { term.dispose(); }
